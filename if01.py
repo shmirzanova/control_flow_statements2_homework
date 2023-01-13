1c159401a3004336c57adb82cd0869cab6d82025
shmirzanova <shoxsanammirzanova76@gmail.com>
2023-01-13 16:39:44 +0500

    uy ishi

--- if01.py
+++ if01.py
@@ -8,4 +8,16 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+
+
+    if a>b and a>c:
+        return a
+    
+    if b>a and b>c:
+        return b
+
+    if c>a and c>b:
+        return c
+    
+
+print(main(9,11,3))
